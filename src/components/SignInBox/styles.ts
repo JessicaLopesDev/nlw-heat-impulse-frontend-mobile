eb45b9997d60a2b4a8531557f3d670c9e46f8738
JessicaLopesDev <jessicalopes.dev@gmail.com>
2021-10-21 17:13:47 -0300

    signinbox component done

--- src/components/SignInBox/styles.ts
+++ src/components/SignInBox/styles.ts
@@ -1,6 +1,12 @@
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import styled, { css } from 'styled-components/native';
 
 export const Container = styled.View`
   ${({ theme }) => css`
+    height: 48px;
+    padding: 0 20px;
+    padding-bottom: ${getBottomSpace() + 32}px;
+    align-items: center;
+    justify-content: center;
   `}
 `;
